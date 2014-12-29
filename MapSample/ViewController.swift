37ef0672ea70284b4df2f2ea10648f47a3b4e24b
hiedar68 <hiedar68@bmnm2827000299.local>
2014-12-29 11:13:24 +0900

    - Import Mapkit - View Map on UIView

--- MapSample/ViewController.swift
+++ MapSample/ViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myMapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
