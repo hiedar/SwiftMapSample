ade33c44ee51c41142225f5c56720410374902e2
hiedar68 <hiedar68@bmnm2827000299.local>
2014-12-29 11:08:15 +0900

    Initial Commit

---
+++ MapSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MapSample
+//
+//  Created by hiedar68 on 2014/12/29.
+//  Copyright (c) 2014年 hiedar68. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
